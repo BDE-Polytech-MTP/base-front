99a1d37c974843187eda172177de5ac63a5f4b8a
Florent HUGOUVIEUX <florent.hugouvieux@protonmail.com>
2020-06-29 11:11:06 +0200

    Remove card and list usage for forms
    Now use flex display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatListModule } from '@angular/material/list';
-import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
@@ -58,13 +55,10 @@ import { FooterComponent } from './footer/footer.component';
     MatSelectModule,
     MatSidenavModule,
     MatIconModule,
-    MatCardModule,
     MatButtonModule,
     FormsModule,
     MatInputModule,
     MatFormFieldModule,
-    MatGridListModule,
-    MatListModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     BrowserAnimationsModule
