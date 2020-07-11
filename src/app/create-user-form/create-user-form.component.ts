59686714a8b285e0c31b2b27a0ce82f5f5551b79
Florent HUGOUVIEUX <florent.hugouvieux@protonmail.com>
2020-07-11 16:59:48 +0200

    Use ReactiveForm instead of Template-drive forms and add errors hints

--- src/app/create-user-form/create-user-form.component.ts
+++ src/app/create-user-form/create-user-form.component.ts
@@ -2,6 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { BdeService } from '../services/bde.service';
 import { UsersService } from '../services/users.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { FormGroup, FormControl, Validators, ValidatorFn, ValidationErrors, AbstractControl } from '@angular/forms';
+import { AppValidators } from '../validators/app-validators';
+
+function minLengthIfNotEmpty(minLength: number): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const actualLength: number = control.value.trim().length;
+    return actualLength > 0 && actualLength < minLength ? { min: { min: minLength, actual: actualLength } } : null;
+  };
+}
 
 @Component({
   selector: 'app-create-user-form',
@@ -12,12 +21,28 @@ export class CreateUserFormComponent implements OnInit {
 
   bdes: { name: string, uuid: string }[] = [];
 
-  data = {
-    bde: '',
-    email: '',
-    firstname: '',
-    lastname: '',
-  };
+  createUserForm = new FormGroup({
+    bde: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    firstname: new FormControl('', [minLengthIfNotEmpty(2), Validators.maxLength(15)]),
+    lastname: new FormControl('', [minLengthIfNotEmpty(2), Validators.maxLength(15)]),
+  });
+
+  get bde() {
+    return this.createUserForm.get('bde');
+  }
+
+  get email() {
+    return this.createUserForm.get('email');
+  }
+
+  get firstname() {
+    return this.createUserForm.get('firstname');
+  }
+
+  get lastname() {
+    return this.createUserForm.get('lastname');
+  }
 
   error?: string;
   success?: string;
@@ -35,7 +60,13 @@ export class CreateUserFormComponent implements OnInit {
     this.sending = true;
     this.error = undefined;
     this.success = undefined;
-    this.usersService.createUser(this.data.bde, this.data.email, this.data.firstname, this.data.lastname).subscribe(
+
+    this.usersService.createUser(
+      this.createUserForm.value.bde,
+      this.createUserForm.value.bde.email,
+      this.createUserForm.value.bde.firstname.trim(),
+      this.createUserForm.value.bde.lastnameµ.trim()
+    ).subscribe(
       () => {
         this.success = 'Compté créé !';
         this.sending = false;
