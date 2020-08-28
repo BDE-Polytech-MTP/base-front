ad6ab1c2c8127f81ea398d4eb29d0630203550c9
Florent HUGOUVIEUX <florent.hugouvieux@protonmail.com>
2020-08-28 18:22:44 +0200

    Change production config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,4 @@ export const environment = {
   production: true
 };
 
-export const API_URL = 'https://bde-api.igpolytech.fr';
+export const API_URL = 'https://bde-polytech-mtp.herokuapp.com'; // For now, later we should use https://api.bde.polytechmontpellier.fr
