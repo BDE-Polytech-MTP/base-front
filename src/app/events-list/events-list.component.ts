af1f54b8e7c84fe065d93676568f7d15ee89d19e
Florent HUGOUVIEUX <florent.hugouvieux@protonmail.com>
2020-09-02 22:00:28 +0200

    "Book" button now enable and disable correctly on date update

--- src/app/events-list/events-list.component.ts
+++ src/app/events-list/events-list.component.ts
@@ -1,38 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { EventsService } from '../services/events.service';
 import { Event } from '../models';
 import { DateTime } from 'luxon';
 import { AuthService } from '../services/auth.service';
+import { Subject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-events-list',
   templateUrl: './events-list.component.html',
   styleUrls: ['./events-list.component.scss']
 })
-export class EventsListComponent implements OnInit {
+export class EventsListComponent implements OnInit, OnDestroy {
 
-  events: Event[] = [];
+  events: (Event & { notBookable: Observable<boolean> })[] = [];
+
+  private timeIntervalSub: number;
+  private timerSubject: Subject<DateTime>;
 
   constructor(private eventsService: EventsService, private authService: AuthService) { }
 
   ngOnInit(): void {
-    this.eventsService.getEvents().subscribe((events) => this.events = events);
+    this.timerSubject = new Subject();
+    this.timeIntervalSub = setInterval(() => {
+      this.timerSubject.next(DateTime.local());
+    });
+    this.eventsService.getEvents().subscribe((events) => this.events = events.map(event => ({
+      ... event,
+      notBookable: this.timerSubject.pipe(map(currentDate => !this.canBook(event, currentDate))),
+    })));
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.timeIntervalSub);
   }
 
   toHumanDate(date: string): string {
     return DateTime.fromISO(date).setLocale('fr').toLocaleString(DateTime.DATETIME_SHORT);
   }
 
-  canBook(event: Event): boolean {
-    if (event.bookingStart && DateTime.fromISO(event.bookingStart) > DateTime.local()) {
+  canBook(event: Event, currentDate: DateTime): boolean {
+    if (event.bookingStart && DateTime.fromISO(event.bookingStart) > currentDate) {
       return false;
     }
 
-    if (event.bookingEnd && DateTime.fromISO(event.bookingEnd) < DateTime.local()) {
+    if (event.bookingEnd && DateTime.fromISO(event.bookingEnd) < currentDate) {
       return false;
     }
 
-    return event.bookingStart !== undefined || event.bookingEnd !== undefined;
+    return true;
   }
 
   canCreateEvent(): boolean {
