f4a5d1b906533a99193cd3e3868218f4ddfc94fd
Florent HUGOUVIEUX <florent.hugouvieux@protonmail.com>
2020-09-08 16:23:10 +0200

    Make table content adapt to screen size

--- src/app/administration/users-list/users-list.component.ts
+++ src/app/administration/users-list/users-list.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { UsersService } from 'src/app/services/users.service';
 import { fromEvent, combineLatest, concat, BehaviorSubject, merge, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { User, UnregisteredUser } from 'src/app/models';
 import { PageEvent } from '@angular/material/paginator';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-users-list',
@@ -13,14 +14,53 @@ import { PageEvent } from '@angular/material/paginator';
 })
 export class UsersListComponent implements OnInit {
 
-  columnsToDisplay = ['email', 'firstname', 'lastname', 'permissions', 'action'];
+  mobileQuery: MediaQueryList;
+  ipadQuery: MediaQueryList;
+  ipadLandQuery: MediaQueryList;
+  private mediaQueryListener: () => void;
+
+  columnsToDisplay = [];
 
   usersCount = 0;
   users: (User | UnregisteredUser)[] = [];
   pageSize: BehaviorSubject<number> = new BehaviorSubject(50);
   pageIndex: BehaviorSubject<number> = new BehaviorSubject(0);
 
-  constructor(private authService: AuthService, private usersService: UsersService) { }
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService,
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher
+  ) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this.ipadQuery = media.matchMedia('(max-width: 800px)');
+    this.ipadLandQuery = media.matchMedia('(max-width: 1050px');
+    this.mediaQueryListener = () => {
+      this.handleMediaChange();
+      changeDetectorRef.detectChanges();
+    };
+
+    /* Using addListener instead of addEventListener because Safari does no support it */
+    // tslint:disable-next-line: deprecation
+    this.mobileQuery.addListener(this.mediaQueryListener);
+    // tslint:disable-next-line: deprecation
+    this.ipadQuery.addListener(this.mediaQueryListener);
+    // tslint:disable-next-line: deprecation
+    this.ipadLandQuery.addListener(this.mediaQueryListener);
+    this.handleMediaChange();
+  }
+
+  handleMediaChange() {
+    if (this.mobileQuery.matches) {
+      this.columnsToDisplay = ['firstname', 'lastname', 'action'];
+    } else if (this.ipadQuery.matches) {
+      this.columnsToDisplay = ['firstname', 'lastname', 'specialty', 'member', 'action'];
+    } else if (this.ipadLandQuery.matches) {
+      this.columnsToDisplay = ['email', 'firstname', 'lastname', 'specialty', 'member', 'action'];
+    } else {
+      this.columnsToDisplay = ['email', 'firstname', 'lastname', 'specialty', 'permissions', 'member', 'action'];
+    }
+  }
 
   ngOnInit(): void {
     const searchChange = concat(
